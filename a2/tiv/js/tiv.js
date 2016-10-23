ce2da24a9dfa6ff8d10e457b56a72f70b1f9b394
Victor Lecomte <csd3449@csd.uoc.gr>
2016-10-23 12:12:31 +0300

    First commit: Created the js file. The images have been replaced by a button for adding a folder

---
+++ a2/tiv/js/tiv.js
@@ -0,0 +1,16 @@
+function TIV3449() {
+    "use strict";
+
+    function loadImages() {
+        
+    }
+
+    function getLoadedImages() {
+
+    }
+
+    function showLoadedImages(elem) {
+
+    }
+    
+}
