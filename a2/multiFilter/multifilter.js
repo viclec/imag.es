0e592b1be6b77979cc23bd184309ff71c0e66ca7
Victor Lecomte <csd3449@csd.uoc.gr>
2016-11-01 03:39:20 +0200

    Added declaration of currentArray, initialized as array.

---
+++ a2/multiFilter/multifilter.js
@@ -0,0 +1,55 @@
+function makeMultiFilter(array) {
+    'use strict';
+    // What we track
+var currentArray = array;
+// TO COMPLETE!
+    return (function arrayFilterer(pred, callback) {
+// If filter not a function return current Array
+// TO COMPLETE TOGETHER!
+// TO COMPLETE TOGETHER!
+// Filter out things
+// TO COMPLETE!
+// If callback is a function, execute callback
+// TO COMPLETE TOGETHER!
+// TO COMPLETE TOGETHER!
+// TO COMPLETE! We have to return something!
+    });
+}
+// !!! THE CODE THAT FOLLOWS USES THE makeMultiFilter function
+// arrayFilterer can be used to repeatedly filter this input array
+var arrayFilterer = makeMultiFilter([1, 2, 3]);
+// call arrayFilterer to filter out all the numbers not equal to 2
+arrayFilterer(function (elem) {
+    'use strict';
+    return elem !== 2; // check if element is not equal to 2
+}, function (currentArray) {
+    'use strict';
+    console.log(this); // prints [1,2 3]
+    console.log(currentArray); // prints [1, 3]
+});
+// call arrayFilterer filter out all the elements not equal to 3. No callback used
+arrayFilterer(function (elem) {
+    'use strict';
+    return elem !== 3; // check if element is not equal to 3
+});
+// call arrayFilterer with no filter should return the current array
+var currentArray = arrayFilterer();
+console.log('currentArray', currentArray); // prints [1] since we filtered out 2 and 3
+// Since arrayFilterer returns itself filters calls can be chained like:
+function filterTwos(elem) {
+    'use strict';
+    return elem !== 2;
+}
+function filterThrees(elem) {
+    'use strict';
+    return elem !== 3;
+}
+var currentArray = makeMultiFilter([1, 2, 3])(filterTwos)(filterThrees)();
+console.log('currentArray', currentArray); // prints [1] since we filtered out 2 and 3
+// Multiple active filters at the same time
+var arrayFilterer1 = makeMultiFilter([1, 2, 3]);
+var arrayFilterer2 = makeMultiFilter([4, 5, 6]);
+console.log(arrayFilterer1(filterTwos)()); // prints [1,3]
+console.log(arrayFilterer2(filterThrees)()); // prints [4,5,6]
+// Προσοχη αν τρέξετε console.log(array) πρέπει να πάρετε undefined
+// Το ίδιο αν τρέξετε console.log(originalArray) ή console.log(currentArray)
