ad107dc981aef984f8eba22b25ca3e5122e65abb
Victor Lecomte <csd3449@csd.uoc.gr>
2016-11-14 01:11:10 +0200

    Added the ability to change the user's info.

--- a3/js/login.js
+++ a3/js/login.js
@@ -66,6 +66,40 @@ function login() {
     xhr.send(null);
 }
 
+function changeInfo() {
+    'use strict';
+    var user = document.getElementById('user').value,
+        email = document.getElementById('email').value,
+        pass = document.getElementById('pass').value,
+        fName = document.getElementById('fname').value,
+        lName = document.getElementById('lname').value,
+        bdate = document.getElementById('bdate').value,
+        sex,
+        country = document.getElementById('country').value,
+        city = document.getElementById('city').value,
+        other = document.getElementById('other').value,
+        xhr = new XMLHttpRequest(),
+        params = "status=updateInfo&user=" + user + "&email=" + email + "&pass=" + pass + "&fName=" + fName + "&lName=" + lName + "&bdate=" + bdate + "&sex=" + sex + "&country=" + country + "&city=" + city + "&other=" + other;
+    if (document.getElementById('sex_male').checked) {
+        sex = document.getElementById('sex_male').value;
+    } else if (document.getElementById('sex_female').checked) {
+        sex = document.getElementById('sex_female').value;
+    } else if (document.getElementById('sex_undefined').checked) {
+        sex = document.getElementById('sex_undefined').value;
+    }
+    
+    xhr.open('GET', 'MyServlet?' + params, true);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            document.getElementById('userForm')
+                    .innerHTML = xhr.responseText;
+        } else if (xhr.status !== 200) {
+            alert('Request failed. Returned status of ' + xhr.status);
+        }
+    };
+    xhr.send(null);
+}
+
 function logout() {
     'use strict';
     var xhr = new XMLHttpRequest(),
