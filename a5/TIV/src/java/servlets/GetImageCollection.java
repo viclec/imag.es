02b553725e1b6e028b9ea03bca5ec3a4c467b077
Antonis Poulakis <antwnic4@hotmail.com>
2016-12-07 00:28:26 +0200

    Added functionality for GetImageCollection servlet.

---
+++ a5/TIV/src/java/servlets/GetImageCollection.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import cs359db.db.PhotosDB;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author antwnis4
+ */
+@WebServlet(name = "GetImageCollection", urlPatterns = {"/GetImageCollection"})
+public class GetImageCollection extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ClassNotFoundException {
+
+        HttpSession session = request.getSession(true);
+        String user = request.getParameter("user");
+        int number;
+        List<Integer> photos;
+
+        if (request.getParameter("number") == null) {
+            number = 10;
+        } else {
+            number = Integer.parseInt(request.getParameter("number"));
+        }
+        if (user != null) {
+            photos = PhotosDB.getPhotoIDs(number, user);
+        } else {
+            photos = PhotosDB.getPhotoIDs(number);
+        }
+        response.setContentType("application/json");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("{\"Photos:[");
+            int i = number;
+            for (Integer key : photos) {
+                out.println("{\"" + key + "\"}");
+                if (--i > 0) {
+                    out.println(",");
+
+                }
+            }
+            out.println("]}");
+
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException e) {
+
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException e) {
+
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
