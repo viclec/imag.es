92d947501ca63d4a1e16138135b2a46c5f4c1483
Antonis Poulakis <antwnic4@hotmail.com>
2016-12-09 14:27:48 +0200

    Minor code fixes.

--- a5/TIV/src/java/servlets/GetImageCollection.java
+++ a5/TIV/src/java/servlets/GetImageCollection.java
@@ -53,7 +53,7 @@ public class GetImageCollection extends HttpServlet {
         response.setContentType("application/json");
         try (PrintWriter out = response.getWriter()) {
             out.println("[");
-            for (int k = 0; k < photos.size() - 1; k++) {
+            for (int k = 0; k < photos.size(); k++) {
                 
                 out.println("\"" + photos.get(k) + "\"");
                 System.out.println("\"" + photos.get(k) + "\"");
